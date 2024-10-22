9d15781efbcf14049c067694e9210072321dd446
bongio94 <alezak94@gmail.com>
2024-10-22 10:20:05 +0200

    feat: added favs page

--- lib/src/presentation/views/favs_view.dart
+++ lib/src/presentation/views/favs_view.dart
@@ -1,3 +1,6 @@
+import 'package:amia_assignment/src/data/hive_db.dart';
+import 'package:amia_assignment/src/presentation/theme/typography.dart';
+import 'package:amia_assignment/src/presentation/widgets/dog_details/breed_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -9,11 +12,39 @@ class FavouriteView extends ConsumerStatefulWidget {
 }
 
 class _FavouriteViewState extends ConsumerState<FavouriteView> {
-
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.yellow,
+    final favorites = ref.watch(favoritesProvider);
+
+    if (favorites.isEmpty) {
+      return const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.favorite_border, size: 64, color: Colors.grey),
+            SizedBox(height: 16),
+            AppText.xl('No favorites yet', isLight: true),
+            SizedBox(height: 8),
+            AppText.m('Add some breeds to your favorites!', isLight: true),
+          ],
+        ),
+      );
+    }
+
+    return ListView.builder(
+      shrinkWrap: true,
+      itemCount: favorites.length,
+      itemBuilder: (context, index) {
+        final breed = favorites.keys.toList()[index];
+
+        return Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: BreedTile(
+            breed: favorites.keys.toList()[index],
+            subBreeds: favorites[breed] ?? 0,
+          ),
+        );
+      },
     );
   }
 }
