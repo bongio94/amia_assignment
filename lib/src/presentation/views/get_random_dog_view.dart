291565ff1054413be4667ee15a9a9f27b59e47c2
bongio94 <alezak94@gmail.com>
2024-10-22 10:17:21 +0200

    feat: reworked random dog view with breed selection

--- lib/src/presentation/views/get_random_dog_view.dart
+++ lib/src/presentation/views/get_random_dog_view.dart
@@ -14,41 +14,106 @@ class GetRandomDog extends ConsumerStatefulWidget {
 }
 
 class _GetDogPageState extends ConsumerState<GetRandomDog> {
+  late final ScrollController scrollController;
+
+  @override
+  void initState() {
+    scrollController = ScrollController();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    final dogData = ref.watch(randomDogImageProvider);
+    final screenSize = MediaQuery.sizeOf(context);
+    final scheme = Theme.of(context).colorScheme;
+    final groupValue = ref.watch(groupValueProvider);
+    final breeds = ref.watch(randomDogByBreedProvider);
 
     return Scaffold(
       floatingActionButton: FilledButton.tonal(
-          onPressed: () => ref.refresh(randomDogImageProvider),
-          child: const AppText.m('I want more!')),
-      body: switch (dogData) {
-        AsyncData(:final value) => SingleChildScrollView(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                const Padding(
-                  padding: EdgeInsets.all(32),
+        onPressed:
+            groupValue != null ? () => ref.refresh(randomDogImageProvider(groupValue)) : null,
+        child: const AppText.m('I want more!'),
+      ),
+      body: ListView(
+        controller: scrollController,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Container(
+              clipBehavior: Clip.hardEdge,
+              width: double.maxFinite,
+              height: screenSize.height * 0.3,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                border: Border.all(
+                  color: scheme.primary,
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
-                    clipBehavior: Clip.hardEdge,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                    child: AppImage(image: value.message),
-                  ),
-                ),
-                const SizedBox(height: 16.0),
-              ],
+              ),
+              child: groupValue == null
+                  ? const Center(
+                      child: AppText.l('Select a breed!'),
+                    )
+                  : DogImage(groupValue),
             ),
           ),
-        AsyncError(:final error) => Text('Error: $error'),
-        _ => const Center(
-            child: CircularProgressIndicator.adaptive(),
+          breeds.when(
+            data: (value) {
+              return ListView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: value.message.keys.length,
+                itemBuilder: (context, index) {
+                  return RadioListTile.adaptive(
+                    title: AppText.m(value.message.keys.toList()[index].capitalize()),
+                    value: value.message.keys.toList()[index],
+                    groupValue: groupValue,
+                    tileColor: scheme.surface,
+                    onChanged: (breed) {
+                      ref.read(groupValueProvider.notifier).state = breed;
+                      if (scrollController.hasClients &&
+                          scrollController.position.extentBefore > 0) {
+                        scrollController.animateTo(
+                          0,
+                          duration: const Duration(milliseconds: 300),
+                          curve: Curves.easeInOutCirc,
+                        );
+                      }
+                    },
+                  );
+                },
+              );
+            },
+            error: (error, stack) => Text('Error: $error'),
+            loading: () => const Center(child: CircularProgressIndicator.adaptive()),
           ),
-      },
+        ],
+      ),
     );
+  }
+}
+
+final groupValueProvider = StateProvider.autoDispose<String?>((ref) {
+  return null;
+});
+
+class DogImage extends ConsumerWidget {
+  final String groupValue;
+  const DogImage(this.groupValue, {super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final imageByBreed = ref.watch(randomDogImageProvider(groupValue));
+
+    return switch (imageByBreed) {
+      AsyncData(:final value) => ClipRRect(
+          borderRadius: BorderRadius.circular(19),
+          child: AppImage(image: value.message),
+        ),
+      AsyncError(:final error) => Text('Error: $error'),
+      _ => const Center(
+          child: CircularProgressIndicator.adaptive(),
+        ),
+    };
   }
 }
